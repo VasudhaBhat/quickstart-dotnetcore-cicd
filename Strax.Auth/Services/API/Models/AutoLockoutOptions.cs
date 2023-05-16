9086ac94097dafd5df3064cd4148219e2d7e992e
VasudhaBhat <vasudhab@straxcorp.com>
2023-05-16 13:38:46 +1000

    remove extra projects from repo

--- Strax.Auth/Services/API/Models/AutoLockoutOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace API.Models
-{
-    public class AutoLockoutOptions
-    {
-        public int DefaultLockoutTimeSpan { get; set; }
-        public int MaxFailedAccessAttempts { get; set; }
-        public bool AllowedForNewUsers { get; set; }
-    }
-}
